c251300d58f1d96467b6c352e76ea97e6f65a37b
jnewman <jnewman@cs.umass.edu>
2014-12-15 17:54:10 -0500

    Fixed some assumptions made with quickAuth that was causing old html5 problems to fail.

--- woServer/resources/js/tutorhut.js
+++ woServer/resources/js/tutorhut.js
@@ -438,7 +438,7 @@ function showHTMLProblem (pid, solution, resource, mode) {
     var dir = resource.split(".")[0];
     // the name of the problem (e.g. problem090.html) is stripped off to find a directory (e.g. problem090)
     if (!isDemo)  {
-        if (!globals.form==="quickAuth")  {
+        if (globals.form!=="quickAuth")  {
             loadIframe(PROBLEM_WINDOWID, sysGlobals.problemContentPath + "/html5Probs/" + dir + "/" + resource);
         }
         else {
@@ -454,7 +454,7 @@ function showHTMLProblem (pid, solution, resource, mode) {
         $(PROBLEM_WINDOWID).attr("domain", sysGlobals.problemContentDomain);
     }
     else {
-        if (!globals.form==="quickAuth") {
+        if (!globals.form!=="quickAuth") {
             loadIframe(EXAMPLE_FRAMEID, sysGlobals.problemContentPath + "/html5Probs/" + dir + "/" + resource);
         }
         else {
@@ -549,6 +549,14 @@ function processNextProblemResult(responseText, textStatus, XMLHttpRequest) {
                 globals.questionImage = activity.questionImage;
                 globals.hints = activity.hints;
                 globals.answers = activity.answers;
+            }
+            else {
+                globals.form = null;
+                globals.statementHTML = null;
+                globals.questionAudio = null;
+                globals.questionImage = null;
+                globals.hints = null;
+                globals.answers = null;
             }
             sendBeginEvent(globals);
             showHTMLProblem(pid,solution,resource,mode);
