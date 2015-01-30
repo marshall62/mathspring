818e3f55ad65a670867cd78a52be250958ec1f13
marshall <marshall@cs.umass.edu>
2015-01-30 12:52:18 -0500

    Added Melissa's quickAuth engine to the system.
    Renamed files to not use _mf and located files in correct locations.

--- woServer/resources/js/problemUtils.js
+++ woServer/resources/js/problemUtils.js
@@ -1,21 +1,49 @@
-var hintStop=true;
+/**
+ * Created with IntelliJ IDEA.
+ * User: Melissa
+ * Date: 12/12/14
+ * Time: 6:46 PM
+ * To change this template use File | Settings | File Templates.     s
+ */
+
 var debug=false;
-var readAloud = false;
 var isShortAnswer=false;
 var couldNotShuffle = false;
+var maxHints = 10;
 
-function isDemo() {
-    return window.parent.isDemoMode();
+function probUtilsInit(doc, multiChoice) {
+    if (typeof multiChoice !== 'undefined')
+        isShortAnswer = !multiChoice;
+    if (!isShortAnswer)
+        shuffleAnswers(doc);
 }
 
-function debugAlert(msg) {
-    if (debug ) {
-        alert(msg);
+function shuffleAnswers(doc) {
+    var oldAnswer = window.parent.getAnswer();
+    var newAnswer = window.parent.getNewAnswer();
+
+    var oldAnsText = getElementCorrespondingToAns(oldAnswer);
+    var newAnsText = getElementCorrespondingToAns(newAnswer);
+    if (oldAnsText === "" || newAnsText === "" ) {
+        couldNotShuffle = true;
+        return;
     }
-}
 
-function getEdgeCompositionId () {
-    return "EdgeProblem";    // hardwired for now
+
+    var ansSel = doc.getElementById(oldAnsText);
+    var newAnsSel = doc.getElementById(newAnsText);
+
+    if(ansSel == null || newAnsSel == null){
+        couldNotShuffle = true;
+        return;
+    }
+
+    var ansContent = ansSel.innerHTML;
+    var otherContent = newAnsSel.innerHTML;
+    var temp = ansContent;
+
+    ansSel.innerHTML = otherContent;
+    newAnsSel.innerHTML = temp;
 }
 
 function getElementCorrespondingToAns(ans) {
@@ -53,40 +81,7 @@ function getElementCorrespondingToAns(ans) {
     }
 }
 
-function shuffleAnswers(sym) {
-    var oldAnswer = window.parent.getAnswer();
-    var newAnswer = window.parent.getNewAnswer();
-
-    var oldAnsText = getElementCorrespondingToAns(oldAnswer);
-    var newAnsText = getElementCorrespondingToAns(newAnswer);
-
-    if (oldAnsText === "" || newAnsText === "" ) {
-        couldNotShuffle = true;
-        return;
-    }
-    var ansSel = sym.lookupSelector(oldAnsText);
-    var newAnsSel = sym.lookupSelector(newAnsText);
-    if (!$(ansSel).length || !$(newAnsSel).length) {
-        couldNotShuffle = true;
-        return;
-    }
-    var ansContent = $(ansSel).html();
-    var otherContent = $(newAnsSel).html();
-    var temp = ansContent;
-    $(ansSel).html(otherContent);
-    $(newAnsSel).html(temp);
-
-}
-
-function probUtilsInit(sym, multiChoice) {
-    if (typeof multiChoice !== 'undefined')
-        isShortAnswer = !multiChoice;
-    if (!isShortAnswer)
-        shuffleAnswers(sym);
-    maybeStop(sym);
-}
-
-function answerClicked (sym, buttonName) {
+function answerClicked (doc, buttonName) {
     if (couldNotShuffle) {
         oldAnswer = window.parent.getAnswer();
         newAnswer = window.parent.getNewAnswer();
@@ -95,108 +90,143 @@ function answerClicked (sym, buttonName) {
         }
     }
     debugAlert(buttonName + " was clicked. Calling parent.answerChosen.");
-    window.parent.tutorhut_answerChosen(sym,buttonName);
+    window.parent.tutorhut_answerChosen(doc, buttonName);
 }
 
-
-// given an array of hint labels in the correct order,  play them one at a time.
-function prob_playHintSequence (hintSequence) {
-    hintStop = false;
-    prob_playHintInternal(hintSequence[0]);
-}
-
-function prob_playHint (hintLabel) {
-    hintStop=true;
-    readAloud = true;
-    prob_playHintInternal(hintLabel);
-}
-
-function prob_playHintInternal(hintLabel) {
-//    var wind = document.getElementById("iframe").contentWindow ;
-    // These Composition IDs are different for each problem.   So we need to figure out where to get from.
-//    var Comp = wind.AdobeEdge.getComposition(getEdgeCompositionId());
-    var Comp = window.AdobeEdge.getComposition(getEdgeCompositionId());
-    Comp.getStage().play(hintLabel,true) ;
-}
-
-
-
-function prob_readProblem() {
-    readAloud = true;
-//    var Comp = sysGlobals.childWindow.AdobeEdge.getComposition(getEdgeCompositionId());
-    var Comp = window.AdobeEdge.getComposition(getEdgeCompositionId());
-    Comp.getStage().play("Read Question",true);
-//    embedSound("question");
-}
-
-// sym is the selected button, buttonName is the letter of the multiple choice answer selected
-function prob_gradeAnswer (sym, buttonName, isCorrect, showHint) {
+function prob_gradeAnswer (doc, answerChosen, isCorrect, showHint) {
     debugAlert("gradeAnswer got " + isCorrect);
     if (!isShortAnswer) {
         if (couldNotShuffle) {
             oldAnswer = window.parent.getAnswer();
             newAnswer = window.parent.getNewAnswer();
-            if (oldAnswer && newAnswer && buttonName.toUpperCase() === oldAnswer.toUpperCase()) {
-                buttonName = newAnswer.toUpperCase(); // TODO: This logic should maybe be in the server
+            if (oldAnswer && newAnswer && answerChosen.toUpperCase() === oldAnswer.toUpperCase()) {
+               answerChosen = newAnswer.toUpperCase(); // TODO: This logic should maybe be in the server
             }
         }
+        answerChosen = answerChosen.toUpperCase();
         if (isCorrect)
         {
-            sym.stop("Answer Correct");
-            if (buttonName != "A")
-                sym.getComposition().getStage().getSymbol("Answers").getSymbol("AButton").stop("Nothing Selected");
-            if (buttonName != "B")
-                sym.getComposition().getStage().getSymbol("Answers").getSymbol("BButton").stop("Nothing Selected");
-            if (buttonName != "C")
-                sym.getComposition().getStage().getSymbol("Answers").getSymbol("CButton").stop("Nothing Selected");
-            if (buttonName != "D")
-                sym.getComposition().getStage().getSymbol("Answers").getSymbol("DButton").stop("Nothing Selected");
-            if (buttonName != "E")
-                sym.getComposition().getStage().getSymbol("Answers").getSymbol("EButton").stop("Nothing Selected");
+            doc.getElementById("AX").style.display = "none";
+            doc.getElementById("BX").style.display = "none";
+            doc.getElementById("CX").style.display = "none";
+            doc.getElementById("DX").style.display = "none";
+            doc.getElementById("EX").style.display = "none";
+            doc.getElementById(answerChosen+'Check').style.display = "initial";
         }
         else
         {
-            sym.stop("Answer Incorrect");
+            doc.getElementById(answerChosen+"X").style.display = "initial";
         }
     }
     else {
         if (isCorrect) {
-            $(sym.lookupSelector("Grade_X")).hide(); // hide it
-            $(sym.lookupSelector("Grade_Check")).show();
+            doc.getElementById("Grade_X").style.display="none"; // hide it
+            doc.getElementById("Grade_Check").style.display="initial";
         }
         else {
-            $(sym.lookupSelector("Grade_Check")).hide(); // hide it
-            $(sym.lookupSelector("Grade_X")).show();
+            doc.getElementById("Grade_Check").style.display="none"; // hide it
+            doc.getElementById("Grade_X").style.display="initial";
         }
     }
 
 }
 
-function maybeStop(sym) {
-    sym.stop();
+function processShortAnswer(doc, ans) {
+    window.parent.tutorhut_shortAnswerSubmitted(doc,ans);
 }
 
-function embedSound (soundFile) {
-    var snd = new Audio();
-    parent.document.getElementById("questionogg").setAttribute("src", "../problem_"+window.parent.globals.resource+"/"+soundFile+".ogg");
-    parent.document.getElementById("questionmp3").setAttribute("src", "../problem_"+window.parent.globals.resource+"/"+soundFile+".mp3");
-    parent.document.getElementById("questionaudio").load();
-    parent.document.getElementById("questionaudio").play();
-}
-
-function processShortAnswer(sym, ans) {
-    window.parent.tutorhut_shortAnswerSubmitted(sym,ans);
-}
-
-function playSound (sym, soundFile) {
-	var soundFileList = soundFile.split(".");
-    // As it stands there's no difference in the code between hitting the 'readProblem' button and automatically playing the problem
-    // Thus, we have to check if the sound file being played is the question sound file. If it is, then we check a flag readAloud
-    // which is true only if the button was clicked.
-    if (!(soundFileList[0].toUpperCase() === "QUESTION") || readAloud == true) {
-        sym.$("QuestionSound_Container").html ("");
-        var questionsound=$("<audio controls autoplay><source src='"+soundFileList[0]+".ogg' type='audio/ogg'><source src='"+soundFileList[0]+".mp3' type='audio/mpeg'>Your browser does not support the audio element.</audio>");
-        sym.$("QuestionSound_Container").append(questionsound);
+function debugAlert(msg) {
+    if (debug ) {
+        alert(msg);
     }
-    readAloud = false;
+}
+
+function prob_readProblem() {
+    stopAudio();
+    document.getElementById("QuestionSound").play();
+}
+
+function prob_playHint (hintLabel) {
+    hint = getElementCorrespondingToHint(hintLabel);
+    clearHintStage();
+    stopAudio();
+    document.getElementById(hint+"Thumb").style.display = "initial";
+    document.getElementById(hint+"ThumbImg").style.display = "none";
+    document.getElementById(hint+"ThumbImgPressed").style.display = "initial";
+    document.getElementById(hint).style.display = "initial";
+    //Animate the hints coming in.  We want them to alternate sliding in from the left or the bottom
+/*    if(hint == "Hint10"){
+        document.getElementById(hint).className = "hint default";
+    }
+    else{
+        requestAnimationFrame(function(){
+            if(hint == "Hint1" ||
+                hint == "Hint3" ||
+                hint == "Hint5" ||
+                hint == "Hint7" ||
+                hint == "Hint9"){
+                document.getElementById(hint).className = "hint slide_left";
+            }
+            else{
+                document.getElementById(hint).className = "hint slide_up";
+            }
+        }, document.getElementById(hint));
+    }
+  */
+    document.getElementById(hint+"Sound").play();
+
+}
+
+function clearHintStage(){
+    for(i = 1; i <= maxHints; ++i){
+        document.getElementById("Hint"+i.toString()).className = "hint default";
+        document.getElementById("Hint"+i.toString()).style.display = "none";
+        document.getElementById("Hint"+i+"ThumbImg").style.display = "initial";
+        document.getElementById("Hint"+i+"ThumbImgPressed").style.display = "none";
+    }
+}
+
+function stopAudio(){
+    if(document.getElementById("QuestionSound").readyState > 0){
+        document.getElementById("QuestionSound").pause();
+        document.getElementById("QuestionSound").currentTime = 0;
+    }
+    for(i = 1; i <= maxHints; ++i){
+        if(document.getElementById("Hint"+i+"Sound").readyState > 0){
+            document.getElementById("Hint"+i+"Sound").pause();
+            document.getElementById("Hint"+i+"Sound").currentTime = 0;
+        }
+    }
+}
+
+function getElementCorrespondingToHint(hintLabel){
+    if (window.parent.getForm() === "quickAuth") {
+        switch (hintLabel) {
+            case "Hint 1":
+                return "Hint1";
+            case "Hint 2":
+                return "Hint2";
+            case "Hint 3":
+                return "Hint3";
+            case "Hint 4":
+                return "Hint4";
+            case "Hint 5":
+                return "Hint5";
+            case "Hint 6":
+                return "Hint6";
+            case "Hint 7":
+                return "Hint7";
+            case "Hint 8":
+                return "Hint8";
+            case "Hint 9":
+                return "Hint9";
+            case "Show Answer":
+                return "Hint10";
+            default:
+                return "";
+        }
+    }
+    else{
+        return hintLabel;
+    }
 }
