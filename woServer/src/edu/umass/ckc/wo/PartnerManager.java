fcb600db76670283ee32df36b8a099085526b4c9
Melissa <mfrechet@cs.umass.edu>
2015-03-16 10:00:26 -0400

    Partner Manager added

---
+++ woServer/src/edu/umass/ckc/wo/PartnerManager.java
@@ -0,0 +1,165 @@
+package edu.umass.ckc.wo;
+
+import edu.umass.ckc.wo.beans.Topic;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Melissa
+ * Date: 2/26/15
+ * Time: 7:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PartnerManager {
+
+    private static HashMap<Integer, WaitingStudent> requesters = new HashMap<Integer, WaitingStudent>();
+    private static HashMap<Integer, HashSet<Integer>> requestees_requesters = new HashMap<Integer, HashSet<Integer>>();
+    private static HashMap<Integer, Integer> current_matches = new HashMap<Integer, Integer>();
+
+
+    public synchronized static void addRequest(Connection conn, int id, ArrayList<String> conditions) throws SQLException{
+        WaitingStudent waiter = new WaitingStudent();
+        waiter.setPartners(conn, id, conditions);
+        ArrayList<Integer> prospPartners = waiter.getPossiblePartners();
+        requesters.put(id, waiter);
+        for(Integer i : prospPartners){
+            if(requestees_requesters.containsKey(i)){
+                requestees_requesters.get(i).add(id);
+            }
+            else{
+                requestees_requesters.put(i, new HashSet<Integer>());
+                requestees_requesters.get(i).add(id);
+            }
+        }
+    }
+
+    public synchronized static Integer checkForRequestingPartner(int id){
+        if(requestees_requesters.containsKey(id)){
+            Integer partner = requestees_requesters.get(id).iterator().next();
+            requesters.get(partner).setPartner(id);
+            current_matches.put(partner, id);
+            ArrayList<Integer> toRemove = requesters.get(partner).getPossiblePartners();
+            for(Integer rem : toRemove){
+                requestees_requesters.get(rem).remove(partner);
+                if(requestees_requesters.get(rem).isEmpty()){
+                    requestees_requesters.remove(rem);
+                }
+            }
+            return partner;
+        }
+        else{
+            return null;
+        }
+    }
+
+    public synchronized static Integer getRequestedPartner(int id){
+        return requesters.get(id).getPartner();
+    }
+
+    public synchronized static void removeRequest(int id){
+        requesters.remove(id);
+        current_matches.remove(id);
+    }
+
+    public synchronized static boolean requestExists(int id) {
+        return requesters.containsKey(id);
+    }
+
+    public synchronized static boolean isPartner(int id){
+        if(current_matches.containsValue(id)){
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized static String getPartnerName(Connection conn, int id) throws SQLException{
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String toReturn;
+        try {
+            String q = "select fname from student where id=?";
+            ps = conn.prepareStatement(q);
+            ps.setInt(1,id);
+            rs = ps.executeQuery();
+            rs.next();
+
+            toReturn = rs.getString("fname");
+            rs.next();
+        } finally {
+            if (rs != null)
+                rs.close();
+            if (ps != null)
+                ps.close();
+
+        }
+        return toReturn;
+    }
+
+    //TODO update partners method
+
+    public static boolean hasEligiblePartners(Connection conn, int id, ArrayList<String> conditions) throws SQLException{
+        WaitingStudent waiter = new WaitingStudent();
+        waiter.setPartners(conn, id, conditions);
+        return !waiter.getPossiblePartners().isEmpty();
+    }
+
+
+    private static class WaitingStudent{
+        private ArrayList<Integer> possiblePartners;
+        private Integer partner = null;
+
+        private void setPartner(Integer partner){
+            this.partner = partner;
+        }
+
+        private Integer getPartner(){
+            return partner;
+        }
+
+        private ArrayList<Integer> getPossiblePartners(){
+            return possiblePartners;
+        }
+
+        private void setPartners(Connection conn, int id, ArrayList<String> conditions) throws SQLException {
+            if(conditions.isEmpty()){
+                possiblePartners = getNeighbors(conn, id);
+            }
+        }
+
+        private ArrayList<Integer> getNeighbors(Connection conn, int id) throws SQLException {
+            ArrayList<Integer> partners = new ArrayList<Integer>();
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            try {
+                String q = "select leftStudId, rightStudId from flankinguser where studId=?";
+                ps = conn.prepareStatement(q);
+                ps.setInt(1,id);
+                rs = ps.executeQuery();
+                rs.next();
+                Integer left = rs.getInt("leftStudId");
+                Integer right = rs.getInt("rightStudId");
+                //This will be a problem if a  studIds is ever 0.
+                if(left != 0){
+                    partners.add(left);
+                }
+                if(right != 0){
+                    partners.add(right);
+                }
+            } finally {
+                if (rs != null)
+                    rs.close();
+                if (ps != null)
+                    ps.close();
+
+            }
+            return partners;
+        }
+    }
+}
