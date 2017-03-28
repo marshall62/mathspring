6d56a228b7a07ab4048af5bc83eb477b4bedd988
marshall62 <marshall62@gmail.com>
2017-03-28 18:02:58 -0400

    insert file that was missing

---
+++ woServer/src/edu/umass/ckc/wo/event/admin/AdminSetTopicModelParametersEvent.java
@@ -0,0 +1,69 @@
+package edu.umass.ckc.wo.event.admin;
+
+import ckc.servlet.servbase.ServletParams;
+
+/**
+ * <p> Created by IntelliJ IDEA.
+ * User: david
+ * Date: March 2017
+ * Time: 9:53
+ * */
+public class AdminSetTopicModelParametersEvent extends AdminEditTopicsEvent {
+
+    private int maxNumProbsPerTopic;
+    private int maxTimeInTopic;
+    private int contentFailureThreshold;
+    private int externalActivityTimeThreshold;
+    private double topicMastery;
+    private int minNumProbsPerTopic;
+    private int minTimeInTopic;
+    private int difficultyRate;
+
+
+
+    public AdminSetTopicModelParametersEvent(ServletParams p) throws Exception {
+        super(p);
+        maxNumProbsPerTopic = p.getInt("maxNumberProbsPerTopic");
+        minNumProbsPerTopic = p.getInt("minNumberProbsPerTopic");
+        maxTimeInTopic = p.getInt("maxTimeInTopic");
+        minTimeInTopic = p.getInt("minTimeInTopic");
+        contentFailureThreshold = p.getInt("contentFailureThreshold");
+        externalActivityTimeThreshold = p.getInt("externalActivityTimeThreshold");
+        topicMastery = p.getDouble("mastery");
+        difficultyRate =p.getInt("difficultyRate");
+    }
+
+   
+
+    public int getMaxNumProbsPerTopic() {
+        return maxNumProbsPerTopic;
+    }
+
+    public int getMaxTimeInTopic() {
+        return maxTimeInTopic;
+    }
+
+    public int getContentFailureThreshold() {
+        return contentFailureThreshold;
+    }
+
+    public double getTopicMastery() {
+        return topicMastery;
+    }
+
+    public int getMinNumProbsPerTopic() {
+        return minNumProbsPerTopic;
+    }
+
+    public int getMinTimeInTopic() {
+        return minTimeInTopic;
+    }
+
+    public int getDifficultyRate() {
+        return difficultyRate;
+    }
+
+    public int getExternalActivityTimeThreshold() {
+        return externalActivityTimeThreshold;
+    }
+}
